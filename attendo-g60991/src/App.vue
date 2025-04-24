948183678039013e3fc0994eae4bf533db9dfe50
Negue abla <60991@etu.he2b.be>
2025-04-24 14:52:20 +0200

    Structure le header

--- attendo-g60991/src/App.vue
+++ attendo-g60991/src/App.vue
@@ -5,26 +5,22 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <!--<h1 class="bg-sky-400"> Hello, Tailwindcss</h1>-->
   <div class="min-h-screen">
-    <!-- Header noir avec titre centré violet -->
     <header class="bg-black py-4">
       <h1 class="text-center text-fuchsia-600 text-3xl font-bold">Attendo</h1>
     </header>
-    <!-- Barre de navigation grise -->
     <nav class="flex justify-between items-center bg-gray-100 px-6 py-2 border-b">
-      <!-- Liens gauche -->
+
       <div class="space-x-6 text-gray-800 text-sm">
         <RouterLink to="/" class="hover:underline">Accueil</RouterLink>
         <RouterLink to="/sessions" class="hover:underline">Sessions</RouterLink>
         <RouterLink to="/about" class="hover:underline">À propos</RouterLink>
       </div>
 
-      <!-- Bouton droit -->
       <button class="border border-gray-400 text-sm px-3 py-1 rounded hover:bg-gray-200">
         Connexion avec Google
       </button>
     </nav>
 
-    <!-- Contenu des pages -->
     <main class="p-6">
       <RouterView />
     </main>
